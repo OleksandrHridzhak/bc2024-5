6c6c27c29f26c5b955a235cb1bb5aad99386c84d
Гріджак Олександр bc2024-5 <oleksandr.hridzhak@lnu.edu.ua>
2024-11-02 12:09:08 +0200

    Додав методи PUT і DELETE

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const options = program.opts();
 const host = options.host;
 const port = options.port;
 const cache = options.cache;
-
+app.use(express.json());
+app.use(express.text());
 //SERVER ---
 app.get('/notes/:name',async (req,res) => {
     const noteName = req.params.name; 
@@ -36,10 +37,44 @@ app.get('/notes/:name',async (req,res) => {
     }
   
 })
-app.get('/notes',async(req,res)=>{
+app.get('/notes', async(req,res)=>{
     const data = await fs.promises.readFile(`${cache}/notes.json`, 'utf8'); 
     const notes = JSON.parse(data)
     res.status(200).json(notes)
+});
+
+app.put('/notes/:name', async (req, res) => {
+    const noteName = req.params.name;
+    const bodyText = req.body; 
+
+    const data = await fs.promises.readFile(`${cache}/notes.json`, 'utf8');
+    const notes = JSON.parse(data);
+
+    for (let noteItem of notes) {
+        if (noteItem.name === noteName) {
+            noteItem.text = bodyText; 
+            await fs.promises.writeFile(`${cache}/notes.json`, JSON.stringify(notes, null, 2));
+            return res.status(200).send(); 
+        }
+    }
+    res.status(404).send(); 
+});
+
+app.delete('/notes/:name',async(req,res)=>{
+    const noteName = req.params.name;
+
+    const data = await fs.promises.readFile(`${cache}/notes.json`, 'utf8');
+    const notes = JSON.parse(data);
+
+    for (let noteItem of notes) {
+        if (noteItem.name === noteName) {
+            noteIndex = notes.indexOf(noteItem);
+            notes.splice(noteIndex,1)
+            await fs.promises.writeFile(`${cache}/notes.json`, JSON.stringify(notes, null, 2));
+            return res.status(200).send(); 
+        }
+    }
+    res.status(404).send(); 
 });
 
 
